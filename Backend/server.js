5b0ae9d7274fa116a9cd66c79c322cba7138526f
Aswin Rajan <aswinachu3333@gmail.com>
2023-09-01 12:30:34 +0530

    setHeader fixed

--- Backend/server.js
+++ Backend/server.js
@@ -17,11 +17,17 @@ app.use(express.json({
   limit: '30mb'
 }));
 
-const corsOptions = {
-  origin: 'https://api.makermatch.online',
-};
 
-app.use(cors(corsOptions));
+
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-Content-Length');
+  next();
+});
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
